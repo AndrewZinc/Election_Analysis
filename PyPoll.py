3243f2ab079463424f9b523d451bcb395e7b84f2
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-08-12 15:04:08 -0400

    Added election processing logic to PyPoll script.

--- PyPoll.py
+++ PyPoll.py
@@ -1,22 +1,9 @@
 # Add our dependencies
-# Import the datetime class from the datetime module with an alias.
-import datetime as dt
 
 # Import the csv and os modules
 
 import csv
 import os
-from tarfile import ReadError
-
-# Use the now() attribute on the datetime classs to retrieve the current time
-
-now = dt.datetime.now()
-
-# Print the current time.
-
-print(f"The current time is {now}!")
-print("")
-
 
 # file_to_load = os.path.join('Resources', 'election_results.csv')
 # Assign a variable for the import filehandle
@@ -29,19 +16,71 @@ file_to_save = 'C:/Users/azinc/Github/Election_Analysis/Analysis/election_analys
 
 # Open a csv file
 # election_data = open(file_to_load,'r')
-    
-print("")
+
+# Declare and initialize the vote accumulator
+total_votes = 0
+# Declare a list of candidates
+candidate_options = []
+# Declare a dictionary for the candidate-votes
+candidate_votes = {}
+
+# Winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 
 with open(file_to_load) as election_data:
-    # to do - perform analysis - print each row in the CSV file
+    # Begin analysis of the CSV file
+    
     file_reader = csv.reader(election_data)
+    
     # print the Header row
     headers = next(file_reader)
-    print(headers)
+
+    for row in file_reader:
+        # Add the vote for the row
+        total_votes += 1
+        
+        # extract the candidate name from each row
+        candidate_name = row[2]
+        
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list
+             candidate_options.append(candidate_name)
+             
+             # Begin tracking the candidate's votes
+             candidate_votes[candidate_name] =0
+             
+        # Add to the candidate's votes
+        candidate_votes[candidate_name] +=1
+
+# Determine the percentage of votes for each candidate
+# Examine each candidate   
+for candidate_name in candidate_options:
+    # Retrieve their vote tally
+    votes = candidate_votes[candidate_name]
     
-    # for row in csv.reader(election_data):
-    #     print(row[0])
+    # compute the vote percentage
+    vote_percent = float(votes) / float(total_votes) * 100
     
+    # print the candidate name and vote percentage
+    print(f"{candidate_name}: {vote_percent:.1f}% ({votes:,})\n")
+    
+    # Determine election winner
+    if (votes > winning_count) and (vote_percent > winning_percentage):
+        winning_candidate = candidate_name
+        winning_count = votes
+        winning_percentage = vote_percent
+    
+winning_candidate_summary = (
+    f"-------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------------\n")
+
+print(winning_candidate_summary)
 
 
 with open(file_to_save, "w") as outfile:
@@ -49,20 +88,5 @@ with open(file_to_save, "w") as outfile:
 
 
 
-
-
-
-
-
-
 # Close the csv file
 # election_data.close()
-
-
-#The data we need to retrieve.
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won.
-# 5. The winner of the election based on popular vote.
-
